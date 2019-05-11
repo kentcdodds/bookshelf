40c9a7c75b86430705ce1c850d77f57ae243b000
Kent C. Dodds <kent@doddsfamily.us>
2019-05-11 16:08:50 -0600

    checkpoint

--- src/components/book-row.js
+++ src/components/book-row.js
@@ -1,5 +1,8 @@
-import './book-row.scss'
-import React from 'react'
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+import * as mq from '../styles/media-queries'
+import * as colors from '../styles/colors'
+
 import {FaCheckCircle, FaMinusCircle} from 'react-icons/fa'
 import {Link} from '@reach/router'
 import Tooltip from '@reach/tooltip'
@@ -9,32 +12,87 @@ function BookRow({book}) {
   const {title, author, notes, coverImageUrl} = book
   return (
     <div
-      style={{
+      css={{
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'flex-end',
         position: 'relative',
       }}
     >
-      <Link to="/book" className="book-row">
-        <div className="book-row__image">
-          <img src={coverImageUrl} alt={`${title} book cover`} />
+      <Link
+        to="/book"
+        css={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 5fr 2fr 0.2fr',
+          gridGap: '2em',
+          border: `1px solid ${colors.gray}`,
+          color: colors.text,
+          padding: '1.25em',
+          borderRadius: '3px',
+          [mq.small]: {
+            gridTemplateColumns: '1fr',
+          },
+        }}
+      >
+        <div
+          css={{
+            height: 160,
+            [mq.small]: {
+              height: 200,
+            },
+          }}
+        >
+          <img
+            src={coverImageUrl}
+            alt={`${title} book cover`}
+            css={{maxHeight: '100%'}}
+          />
         </div>
         <div>
-          <h2 className="book-row__title">{title}</h2>
+          <h2
+            css={{
+              fontSize: '1.25em',
+              margin: '0',
+            }}
+          >
+            {title}
+          </h2>
           <Rating />
-          <p>{notes}</p>
+          <p
+            css={{
+              marginTop: '0.5em',
+              marginBottom: '0',
+            }}
+          >
+            {notes}
+          </p>
         </div>
         <div className="author author--mt">{author}</div>
       </Link>
-      <div className="book-row__action-buttons">
+      <div
+        css={{
+          marginLeft: '20px',
+          position: 'absolute',
+          color: colors.gray80,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-around',
+          height: '100%',
+        }}
+      >
         <Tooltip label="Mark as read">
-          <button className="button--circle book-row__mark-as-read">
+          <button
+            className="button--circle"
+            css={{':hover,:focus': {color: colors.green}}}
+          >
             <FaCheckCircle />
           </button>
         </Tooltip>
         <Tooltip label="Remove from list">
-          <button className="button--circle book-row__remove-from-list">
+          <button
+            className="button--circle"
+            css={{':hover,:focus': {color: colors.danger}}}
+          >
             <FaMinusCircle />
           </button>
         </Tooltip>
