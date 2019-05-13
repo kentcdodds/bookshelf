6a34781f89b88180517526a31e9d9e74951f3119
Kent C. Dodds <kent@doddsfamily.us>
2019-05-13 06:06:26 -0600

    this is a better place for suspense

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import {useUser} from './context/user-context'
+import {FullPageSpinner} from './components/lib'
 
-const AuthenticatedApp = React.lazy(() => import('./authenticated-app'))
+const loadAuthenticatedApp = () => import('./authenticated-app')
+const AuthenticatedApp = React.lazy(loadAuthenticatedApp)
 const UnauthenticatedApp = React.lazy(() => import('./unauthenticated-app'))
 
 function App() {
   const user = useUser()
-  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />
+  // pre-load the authenticated side in the background while the user's
+  // filling out the login form.
+  React.useEffect(() => {
+    loadAuthenticatedApp()
+  }, [])
+  return (
+    <React.Suspense fallback={<FullPageSpinner />}>
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+    </React.Suspense>
+  )
 }
 
 export default App
--- src/context/index.js
+++ src/context/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import {AuthProvider} from './auth-context'
 import {UserProvider} from './user-context'
-import {FullPageSpinner} from '../components/lib'
 
 function AppProviders({children}) {
   return (
-    <React.Suspense fallback={<FullPageSpinner />}>
-      <AuthProvider>
-        <UserProvider>{children}</UserProvider>
-      </AuthProvider>
-    </React.Suspense>
+    <AuthProvider>
+      <UserProvider>{children}</UserProvider>
+    </AuthProvider>
   )
 }
 
