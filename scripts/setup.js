0a1d9d62fccbcef9f0c15fa4ad55a182d42d1180
Kent C. Dodds <me+github@kentcdodds.com>
2020-03-24 22:27:37 -0600

    fix setup stuff

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,19 +1,43 @@
-var path = require('path')
-var pkg = require(path.join(process.cwd(), 'package.json'))
+var spawnSync = require('child_process').spawnSync
 
-// if you install it then this should be require('workshop-setup')
-// but that... doesn't really make sense.
-require('./workshop-setup')
-  .setup(pkg.engines)
-  .then(
-    () => {
-      console.log(`💯  You're all set up! 👏`)
-    },
-    error => {
-      console.error(`🚨  There was a problem:`)
-      console.error(error)
-      console.error(
-        `\nIf you would like to just ignore this error, then feel free to do so and install dependencies as you normally would in "${process.cwd()}". Just know that things may not work properly if you do...`,
-      )
-    },
+var styles = {
+  blue: {open: '\u001b[34m', close: '\u001b[39m'},
+  dim: {open: '\u001b[2m', close: '\u001b[22m'},
+  red: {open: '\u001b[31m', close: '\u001b[39m'},
+  green: {open: '\u001b[32m', close: '\u001b[39m'},
+}
+
+function color(modifier, string) {
+  return styles[modifier].open + string + styles[modifier].close
+}
+
+console.log(color('blue', '▶️  Starting workshop setup...'))
+
+var error = spawnSync('npx --version', {shell: true}).stderr.toString().trim()
+if (error) {
+  console.error(
+    color(
+      'red',
+      '🚨  npx is not available on this computer. Please install npm@5.2.0 or greater',
+    ),
   )
+  throw error
+}
+
+var command =
+  'npx -q "https://gist.github.com/kentcdodds/bb452ffe53a5caa3600197e1d8005733" --no-autofill'
+console.log(color('dim', '      Running the following command: ' + command))
+
+var result = spawnSync(command, {stdio: 'inherit', shell: true})
+
+if (result.status === 0) {
+  console.log(color('green', '✅  Workshop setup complete...'))
+} else {
+  process.exit(result.status)
+}
+
+/*
+eslint
+  no-var: "off",
+  "vars-on-top": "off",
+*/
