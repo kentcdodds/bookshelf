774f80d89b39e2dc4222b7ba7802a9f88583b35d
Kent C. Dodds <me+github@kentcdodds.com>
2020-03-31 15:44:45 -0600

    stop unregistering service workers i guess

--- src/test/server.js
+++ src/test/server.js
@@ -1,16 +1,6 @@
 import {composeMocks} from 'msw'
 import {handlers} from './server-handlers'
 
-async function setupServer() {
-  try {
-    const registrations = await navigator.serviceWorker.getRegistrations()
-    await Promise.allSettled(registrations.map(r => r.unregister()))
-  } catch (error) {
-    console.error('Service Worker unregistration failed: ', error)
-  }
-
-  // Start the Service Worker
-  return composeMocks(...handlers).start('/mockServiceWorker.js')
-}
-
-window.__bookshelf_serverReady = setupServer()
+window.__bookshelf_serverReady = composeMocks(...handlers).start(
+  '/mockServiceWorker.js',
+)
