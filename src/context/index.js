7e867e8b0c4973125386c4820593a5564dd66a2a
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-14 17:00:21 -0600

    set the basename correctly

--- src/context/index.js
+++ src/context/index.js
@@ -5,6 +5,9 @@ import {homepage} from '../../package.json'
 import {AuthProvider} from './auth-context'
 
 const fullUrl = new URL(homepage)
+const basename = fullUrl.pathname.endsWith('/')
+  ? fullUrl.pathname.slice(0, fullUrl.pathname.length - 1)
+  : fullUrl.pathname
 
 const queryConfig = {
   useErrorBoundary: true,
@@ -14,7 +17,7 @@ const queryConfig = {
 function AppProviders({children}) {
   return (
     <ReactQueryConfigProvider config={queryConfig}>
-      <Router basename={fullUrl.pathname}>
+      <Router basename={basename}>
         <AuthProvider>{children}</AuthProvider>
       </Router>
     </ReactQueryConfigProvider>
