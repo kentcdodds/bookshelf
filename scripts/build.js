9edbf46056dd03f8e628c0f47846e85d22ef8aef
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-14 15:16:55 -0600

    use is-ci to determine whether we are on ci

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,9 @@
+const isCI = require('is-ci')
 const {spawnSync} = require('./utils')
+
 const branch = spawnSync('git rev-parse --abbrev-ref HEAD')
-if (branch === 'master' || !process.env.CI) {
+
+if (branch === 'master' || !isCI) {
   spawnSync('react-scripts build', {stdio: 'inherit'})
 } else {
   spawnSync('node ./scripts/build-variants', {stdio: 'inherit'})
