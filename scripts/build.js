d26013f442345fd2bf4bb2d349b1f69d6aadc104
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-13 22:52:26 -0600

    do not build variants if not on ci

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,6 @@
 const {spawnSync} = require('./utils')
 const branch = spawnSync('git rev-parse --abbrev-ref HEAD')
-if (branch === 'master') {
+if (branch === 'master' || !process.env.CI) {
   spawnSync('react-scripts build', {stdio: 'inherit'})
 } else {
   spawnSync('node ./scripts/build-variants', {stdio: 'inherit'})
