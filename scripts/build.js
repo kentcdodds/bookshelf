fc3bf9f7d2955c2f868bb474ef1ad814935cab6c
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-17 12:38:03 -0600

    add npx for react-scripts build command

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,7 @@ const {spawnSync} = require('./utils')
 const branch = spawnSync('git rev-parse --abbrev-ref HEAD')
 
 if (branch === 'master' || !isCI) {
-  spawnSync('react-scripts build', {stdio: 'inherit'})
+  spawnSync('npx react-scripts build', {stdio: 'inherit'})
 } else {
   spawnSync('node ./scripts/build-variants', {stdio: 'inherit'})
 }
