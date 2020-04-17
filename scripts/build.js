f1ece26a2fa445b126bb0140d3589b414741a387
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-17 17:25:55 -0600

    changeup

--- scripts/build.js
+++ scripts/build.js
@@ -2,8 +2,8 @@ const {spawnSync} = require('./utils')
 
 const branch = spawnSync('git rev-parse --abbrev-ref HEAD')
 
-if (branch === 'master') {
-  spawnSync('npx react-scripts build', {stdio: 'inherit'})
-} else {
+if (branch.startsWith('exercise/')) {
   spawnSync('node ./scripts/build-variants', {stdio: 'inherit'})
+} else {
+  spawnSync('npx react-scripts build', {stdio: 'inherit'})
 }
