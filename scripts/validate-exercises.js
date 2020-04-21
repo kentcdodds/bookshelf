176b7b11d3da31d1b599921305ddc1e31741d0a7
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-21 14:10:05 -0600

    make validate exercises fix things instead

--- scripts/validate-exercises.js
+++ scripts/validate-exercises.js
@@ -1,22 +1,8 @@
-const fs = require('fs')
-const glob = require('glob')
+const {spawnSync, getVariants} = require('./utils')
 
-const files = glob.sync('./src/**/*.*')
+const masterFiles = Object.keys(getVariants())
 
-const errors = []
-for (const file of files) {
-  const contents = fs.readFileSync(file).toString()
-  if (/^export.*\.(exercise|extra-\d+)'$/gim.test(contents)) {
-    errors.push(`This file is exporting a non-final version: ${file}`)
-  }
-  if (
-    /^import.*\.(final|exercise|extra-\d+)'$/gim.test(contents) ||
-    /^\} from '.*\.(final|exercise|extra-\d+)'$/gim.test(contents)
-  ) {
-    errors.push(`This file is importing the wrong version: ${file}`)
-  }
-}
-
-if (errors.length) {
-  throw new Error(errors.join('\n'))
+if (masterFiles.length) {
+  spawnSync('node ./scripts/swap final')
+  spawnSync(`git add "${masterFiles.join('" "')}"`)
 }
