e7cb327fcae039c2ad9932f4952e0551632dff10
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-21 22:40:55 -0600

    add log for CI

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,9 @@ const branch =
   process.env.BRANCH || spawnSync('git rev-parse --abbrev-ref HEAD')
 
 if (branch.startsWith('exercise/')) {
+  console.log(`Building varients for ${branch}`)
   spawnSync('node ./scripts/build-variants', {stdio: 'inherit'})
 } else {
+  console.log(`Building app for ${branch}`)
   spawnSync('npx react-scripts build --profile', {stdio: 'inherit'})
 }
