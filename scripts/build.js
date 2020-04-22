cae8e60718383075ac5cf07c497aa7d8303ad7f8
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-21 22:55:57 -0600

    building fix

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,9 @@
 const {spawnSync} = require('./utils')
 
-const branch = spawnSync('git rev-parse --abbrev-ref HEAD')
+const branch =
+  process.env.BRANCH || spawnSync('git rev-parse --abbrev-ref HEAD')
+
+console.log(`Building for branch "${branch}"`)
 
 if (branch.startsWith('exercises/')) {
   spawnSync('node ./scripts/build-variants', {stdio: 'inherit'})
