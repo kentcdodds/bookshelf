02ce4ac8da074f831ce82695d5dd4f2de8248eec
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-21 22:43:48 -0600

    remove logging

--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,9 @@
 const {spawnSync} = require('./utils')
 
-const branch =
-  process.env.BRANCH || spawnSync('git rev-parse --abbrev-ref HEAD')
+const branch = spawnSync('git rev-parse --abbrev-ref HEAD')
 
 if (branch.startsWith('exercise/')) {
-  console.log(`Building varients for ${branch}`)
   spawnSync('node ./scripts/build-variants', {stdio: 'inherit'})
 } else {
-  console.log(`Building app for ${branch}`)
   spawnSync('npx react-scripts build --profile', {stdio: 'inherit'})
 }
