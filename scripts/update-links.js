5fdb9d5f1cd7805175917b30450d372cb10abed7
Kent C. Dodds <me+github@kentcdodds.com>
2020-06-03 22:09:45 -0600

    add update links script

---
+++ scripts/update-links.js
@@ -0,0 +1,81 @@
+const path = require('path')
+const fs = require('fs')
+const {spawnSync} = require('./utils')
+const pkg = require(path.join(process.cwd(), 'package.json'))
+const {homepage: projectHomepage} = pkg
+
+const branch = spawnSync('git rev-parse --abbrev-ref HEAD')
+const exerciseNumber = branch.match(/^exercises\/(\d+)/)[1]
+
+const contents = fs.readFileSync('INSTRUCTIONS.md', {encoding: 'utf-8'})
+const newContents = [contents]
+  .map(getLinesWithProdDeploys)
+  .map(getLinesWithUpdatedFeedbackLink)[0]
+
+if (contents !== newContents) {
+  fs.writeFileSync('INSTRUCTIONS.md', newContents)
+}
+
+function getLinesWithProdDeploys(contents) {
+  const lines = contents.split('\n')
+  const exerciseProdDeployLines = `
+Production deploys:
+
+- [Exercise](${projectHomepage}exercise)
+- [Final](${projectHomepage})
+`
+    .trim()
+    .split('\n')
+
+  const getExtraDeployLines = extraNumber => [
+    `[Production deploy](${projectHomepage}extra-${extraNumber})`,
+  ]
+
+  const newLines = []
+  for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+    const line = lines[lineIndex]
+    newLines.push(line)
+
+    const extraCreditMatch = line.match(/### (?<number>\d+)\. 💯 /)
+
+    if (/## Exercise$/.test(line)) {
+      newLines.push('', ...exerciseProdDeployLines)
+      if (lines[lineIndex + 2].startsWith('Production deploys:')) {
+        // already existed, skip indexes
+        lineIndex = lineIndex + exerciseProdDeployLines.length + 1
+      }
+    } else if (extraCreditMatch) {
+      const number = extraCreditMatch.groups.number
+      const extraDeployLines = getExtraDeployLines(number)
+      newLines.push('', ...extraDeployLines)
+      if (lines[lineIndex + 2].startsWith('[Production deploy]')) {
+        // already existed, skip indexes
+        lineIndex = lineIndex + extraDeployLines.length + 1
+      }
+    }
+  }
+  return newLines.join('\n')
+}
+
+function getLinesWithUpdatedFeedbackLink(contents) {
+  const feedbackLinkRegex = /^https?:\/\/ws\.kcd\.im.*?&em=$/m
+
+  const firstLine = contents.split('\n')[0]
+  const titleMatch = firstLine.match(/# (?<title>.*)$/)
+  if (!titleMatch) {
+    throw new Error(`Title is invalid`)
+  }
+  const title = titleMatch.groups.title.trim()
+  const workshop = encodeURIComponent('Build React Apps')
+  const exercise = encodeURIComponent(`${exerciseNumber}: ${title}`)
+  const link = `https://ws.kcd.im/?ws=${workshop}&e=${exercise}&em=`
+  if (contents.includes(link)) {
+    return contents
+  }
+  if (!feedbackLinkRegex.test(contents)) {
+    throw new Error(
+      `Exercise "${exerciseNumber}" is missing workshop feedback link`,
+    )
+  }
+  return contents.replace(feedbackLinkRegex, link)
+}
--- scripts/validate-exercises.js
+++ scripts/validate-exercises.js
@@ -4,5 +4,6 @@ const masterFiles = Object.keys(getVariants())
 
 if (masterFiles.length) {
   spawnSync('node ./scripts/swap final')
-  spawnSync(`git add "${masterFiles.join('" "')}"`)
+  spawnSync('node ./scripts/update-links')
+  spawnSync(`git add INSTRUCTIONS.md "${masterFiles.join('" "')}"`)
 }
