b7e948f02ae6bdfa96515c6a5d4c0745c579c375
Kent C. Dodds <me+github@kentcdodds.com>
2020-06-17 16:34:56 -0600

    improve git-diffs script

--- scripts/git-diffs.js
+++ scripts/git-diffs.js
@@ -2,34 +2,26 @@ const {getVariants} = require('./utils')
 
 const variants = getVariants()
 
-function getFilesForVariant(number) {
-  if (number === 'final') {
-    return Object.values(variants)
-      .map(({exercise, final}) => {
-        if (final) {
-          return {
-            before: exercise.file,
-            after: final.file,
-          }
-        }
-        return null
-      })
-      .filter(Boolean)
-  }
-  return Object.values(variants)
-    .map(({exercise, extras}) => {
-      const extra = extras.find(e => e.number === Number(number))
-      if (extra) {
-        return {
-          before: exercise.file,
-          after: extra.file,
-        }
-      }
-      return null
-    })
-    .filter(Boolean)
+const [, , end, start = 'exercise'] = process.argv
+
+function getVariant(variant, requested) {
+  if (variant[requested]) return variant[requested]
+  return variant.extras.find(e => e.number === Number(requested))
 }
 
-for (const {before, after} of getFilesForVariant(process.argv[2])) {
+function getFilesForVariant() {
+  return Object.values(variants)
+    .map(variant => {
+      const before = getVariant(variant, start)
+      const after = getVariant(variant, end)
+      return {
+        before: before ? before.file : null,
+        after: after ? after.file : null,
+      }
+    })
+    .filter(({before, after}) => before && after)
+}
+
+for (const {before, after} of getFilesForVariant()) {
   console.log(`git diff --no-index "${before}" "${after}"`)
 }
