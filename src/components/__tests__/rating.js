e27325e637333f8c2d01d60f3623c91b134636ab
Juliano Farias <juliano@frontendwizard.dev>
2020-10-10 02:09:30 +0200

    test(rating): add unit test for rating component

---
+++ src/components/__tests__/rating.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {render, screen, loginAsUser, act, waitFor} from 'test/app-test-utils'
+import userEvent from '@testing-library/user-event'
+import {buildBook, buildListItem} from 'test/generate'
+import * as booksDB from 'test/data/books'
+import * as listItemsDB from 'test/data/list-items'
+import {Rating} from '../Rating'
+
+async function renderRating({rating = 0} = {}) {
+  const book = await booksDB.create(buildBook())
+  const user = await loginAsUser()
+  const listItem = await listItemsDB.create({
+    ...buildListItem({owner: user, book}),
+    rating,
+  })
+  const utils = await render(<Rating listItem={listItem} />, {user})
+  return {...utils, book, user, listItem}
+}
+
+test('it updates the rating', async () => {
+  const {listItem, rerender} = await renderRating()
+  const firstStar = screen.getByLabelText('1 star')
+
+  userEvent.click(firstStar)
+
+  // needed because of react-query
+  await waitFor(() => {})
+  const updatedListItem = await listItemsDB.read(listItem.id)
+  rerender(<Rating listItem={updatedListItem} />)
+  expect(screen.getByLabelText('1 star')).toBeChecked()
+})
