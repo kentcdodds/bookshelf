65f57f571acc5883807310834635380a07ad8337
Christian Schurr <fischerc.it@gmail.com>
2020-10-18 22:27:13 +0200

    fixed validate and removed solution from exercise

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,5 +1,1 @@
-import {formatDate} from '../misc'
-
-test('formatDate formats the date to look nice', () => {
-  expect(formatDate(new Date('October 18, 1988'))).toBe('Oct 88')
-})
+test.todo('formatDate formats the date to look nice')
