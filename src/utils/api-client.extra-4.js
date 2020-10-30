33504f565c58ebc97de7bca24c385ba8daa81d11
Kent C. Dodds <me+github@kentcdodds.com>
2020-10-30 16:25:45 -0600

    exercises/04-authentication

---
+++ src/utils/api-client.extra-4.js
@@ -0,0 +1,35 @@
+import * as auth from 'auth-provider'
+const apiURL = process.env.REACT_APP_API_URL
+
+function client(
+  endpoint,
+  {data, token, headers: customHeaders, ...customConfig} = {},
+) {
+  const config = {
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': data ? 'application/json' : undefined,
+      ...customHeaders,
+    },
+    ...customConfig,
+  }
+
+  return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      await auth.logout()
+      // refresh the page for them
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please re-authenticate.'})
+    }
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    } else {
+      return Promise.reject(data)
+    }
+  })
+}
+
+export {client}
