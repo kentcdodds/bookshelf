33889747b909a5a5efdc938834b64af77bf71768
Kent C. Dodds <me+github@kentcdodds.com>
2020-11-02 13:12:08 -0700

    exercises/06-cache-management

---
+++ src/utils/books.extra-1.js
@@ -0,0 +1,39 @@
+import {useQuery} from 'react-query'
+import {client} from './api-client'
+import bookPlaceholderSvg from 'assets/book-placeholder.svg'
+
+const loadingBook = {
+  title: 'Loading...',
+  author: 'loading...',
+  coverImageUrl: bookPlaceholderSvg,
+  publisher: 'Loading Publishing',
+  synopsis: 'Loading...',
+  loadingBook: true,
+}
+
+const loadingBooks = Array.from({length: 10}, (v, index) => ({
+  id: `loading-book-${index}`,
+  ...loadingBook,
+}))
+
+function useBookSearch(query, user) {
+  const result = useQuery({
+    queryKey: ['bookSearch', {query}],
+    queryFn: () =>
+      client(`books?query=${encodeURIComponent(query)}`, {
+        token: user.token,
+      }).then(data => data.books),
+  })
+  return {...result, books: result.data ?? loadingBooks}
+}
+
+function useBook(bookId, user) {
+  const {data} = useQuery({
+    queryKey: ['book', {bookId}],
+    queryFn: () =>
+      client(`books/${bookId}`, {token: user.token}).then(data => data.book),
+  })
+  return data ?? loadingBook
+}
+
+export {useBook, useBookSearch}
