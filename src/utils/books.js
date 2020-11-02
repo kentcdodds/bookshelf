b8d5c12b2b13fd5f0cfda990e274109d4a17e2d2
Kent C. Dodds <me+github@kentcdodds.com>
2020-11-02 13:12:08 -0700

    exercises/06-cache-management

--- src/utils/books.js
+++ src/utils/books.js
@@ -1,77 +1,12 @@
-import * as React from 'react'
-import {useQuery, queryCache} from 'react-query'
-import {useClient} from 'context/auth-context'
-import bookPlaceholderSvg from 'assets/book-placeholder.svg'
+// no final
 
-const loadingBook = {
-  title: 'Loading...',
-  author: 'loading...',
-  coverImageUrl: bookPlaceholderSvg,
-  publisher: 'Loading Publishing',
-  synopsis: 'Loading...',
-  loadingBook: true,
-}
+export * from './books.exercise'
 
-const loadingBooks = Array.from({length: 10}, (v, index) => ({
-  id: `loading-book-${index}`,
-  ...loadingBook,
-}))
+// 💯 Make hooks
+// export * from './books.extra-1'
 
-const bookQueryConfig = {
-  staleTime: 1000 * 60 * 60,
-  cacheTime: 1000 * 60 * 60,
-}
+// 💯 Prefetch the book search query
+// export * from './books.extra-5'
 
-const getBookSearchConfig = (client, query) => ({
-  queryKey: ['bookSearch', {query}],
-  queryFn: () =>
-    client(`books?query=${encodeURIComponent(query)}`).then(data => data.books),
-  config: {
-    onSuccess(books) {
-      for (const book of books) {
-        queryCache.setQueryData(
-          ['book', {bookId: book.id}],
-          book,
-          bookQueryConfig,
-        )
-      }
-    },
-  },
-})
-
-function useBookSearch(query) {
-  const client = useClient()
-  const result = useQuery(getBookSearchConfig(client, query))
-  return {...result, books: result.data ?? loadingBooks}
-}
-
-function useBook(bookId) {
-  const client = useClient()
-  const {data} = useQuery({
-    queryKey: ['book', {bookId}],
-    queryFn: () => client(`books/${bookId}`).then(data => data.book),
-    ...bookQueryConfig,
-  })
-  return data ?? loadingBook
-}
-
-function useRefetchBookSearchQuery() {
-  const client = useClient()
-  return React.useCallback(
-    async function refetchBookSearchQuery() {
-      queryCache.removeQueries('bookSearch')
-      await queryCache.prefetchQuery(getBookSearchConfig(client, ''))
-    },
-    [client],
-  )
-}
-
-function setQueryDataForBook(book) {
-  queryCache.setQueryData({
-    queryKey: ['book', {bookId: book.id}],
-    queryFn: book,
-    ...bookQueryConfig,
-  })
-}
-
-export {useBook, useBookSearch, useRefetchBookSearchQuery, setQueryDataForBook}
+// 💯 Add books to the query cache
+// export * from './books.extra-6'
