d2c87d5ac4c9c9df1c43628741a5170d5b98f60f
Kent C. Dodds <me+github@kentcdodds.com>
2020-11-05 11:30:24 -0700

    exercises/02-styles

---
+++ src/__tests__/index.js
@@ -0,0 +1,57 @@
+import chalk from 'chalk'
+import ReactDOM from 'react-dom'
+import {screen, prettyDOM} from '@testing-library/react'
+
+function withMessage(cb, message, {solo = true} = {}) {
+  try {
+    cb()
+  } catch (error) {
+    if (solo) {
+      // eslint-disable-next-line no-throw-literal
+      throw `🚨  ${chalk.reset.red(message)}`
+    } else {
+      error.message = `🚨  ${chalk.reset.red(message)}\n\n${error.message}`
+    }
+    throw error
+  }
+}
+
+test('renders the app', () => {
+  const root = document.createElement('div')
+  root.id = 'root'
+  document.body.append(root)
+
+  require('..')
+
+  screen.getByTitle('Bookshelf')
+  screen.getByRole('heading', {name: /Bookshelf/i})
+  screen.getByRole('button', {name: /Login/i})
+  screen.getByRole('button', {name: /Register/i})
+
+  const cssEl = document.body.querySelector('[css]')
+  withMessage(
+    () => expect(cssEl).toBeNull(),
+    `
+At least one element has an attribute called "css". This means that emotion did not compile the prop correctly.
+
+Make sure to include this at the top of the file:
+
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+
+
+Here's the element that has the css attribute that wasn't compiled:
+
+${prettyDOM(cssEl)}
+    `.trim(),
+  )
+
+  withMessage(
+    () => expect(document.body.querySelector('[class*=css-]')).not.toBeNull(),
+    `None of the elements are styled by emotion. Make sure to render a styled component and use the css prop.`,
+  )
+
+  // cleanup
+  ReactDOM.unmountComponentAtNode(root)
+  document.body.removeChild(root)
+})
