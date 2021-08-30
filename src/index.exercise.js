b22bee3b63be0263a83e40a6268266d9efe6192f
Ronan Moris <ronanmoris@gmail.com>
2021-08-30 09:57:20 +0200

    Bootstrap app

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,17 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+import {Logo} from 'components/logo'
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
+const App = () => {
+  return (
+    <>
+      <Logo />
+      <h1>Bookshelf</h1>
+      <button onClick={() => alert('Login clicked')}>Login</button>
+      <button onClick={() => alert('Register clicked')}>Register</button>
+    </>
+  )
+}
 
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root'))
