2dfe16aa81e2620c9378ede1f1194d03eb4343fe
fsoocie <fsoocie@gmail.com>
2021-11-06 15:18:52 +0300

    add authorization page

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,69 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Logo } from './components/logo';
+import { Dialog } from '@reach/dialog';
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+import "@reach/dialog/styles.css";
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
+const LoginForm = ({ onSubmitHandler, buttonText }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const {username, password} = event.target.elements
+    onSubmitHandler({
+      username: username.value,
+      password: password.value
+    })
+  }
+    return (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id='username' placeholder='Username' />
+          </div>
+          <div>
+            <label htmlFor='password'>Password:</label>
+            <input type="password" id='password' placeholder='Password' />
+          </div>
+            <button>{buttonText}</button>
+        </form>
+    )
+}
 
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+const App = () => {
+    const [openModal, setOpenModal] = React.useState('none');
+    const onLoginHandler = () => setOpenModal('login');
+    const onRegisterHandler = () => setOpenModal('register');
+    const closeModal = () => setOpenModal('none');
+
+    const login = (formData) => {
+      console.log(formData)
+    };
+
+    const register = (formData) => {
+      console.log(formData)
+    }
+
+    return (
+        <div>
+            <Logo />
+
+            <div>
+              <button onClick={onLoginHandler}>Login</button>
+            </div>
+            <div>
+              <button onClick={onRegisterHandler}>Register</button>
+            </div>
+
+            <Dialog aria-label='Login form' isOpen={openModal === 'login'} onDismiss={closeModal}>
+                <h2>Login</h2>
+                <LoginForm onSubmitHandler={login} buttonText='Login!'/>
+            </Dialog>
+            <Dialog aria-label='Registration form' isOpen={openModal === 'register'} onDismiss={closeModal}>
+              <h2>Register</h2>
+              <LoginForm onSubmitHandler={register} buttonText='Register!'/>
+            </Dialog>
+        </div>
+    )
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-export * from './index.final'
+// export * from './index.final'
 
-// export * from './index.exercise'
+export * from './index.exercise'
 
 // 💯 Use `@reach/dialog`
 // export * from './index.extra-1'
