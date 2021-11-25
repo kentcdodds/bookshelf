faa0987ed12e2931dbb3639bd157a8c287af305c
Dean Spooner <dean.spooner@deptagency.com>
2021-11-25 14:35:23 +0000

    Exercise 02 complete.

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -1,71 +1,79 @@
-import styled from '@emotion/styled'
-import {Dialog as ReachDialog} from '@reach/dialog'
+import styled from "@emotion/styled/macro";
+import { keyframes } from "@emotion/core";
+import { FaSpinner } from "react-icons/fa";
+import { Dialog as ReachDialog } from "@reach/dialog";
+import * as colors from "styles/colors";
+import * as mq from "styles/media-queries";
 
-// 🐨 create a button styled component here called "Button"
-// make it look nice and allow it to support a "variant" prop which can be
-// either "primary" or "secondary".
-// 💰 don't forget to export it at the bottom!
-// 💰 In my final version, I style padding, border, lineHeight, and borderRadius
-//    the same for both types, and then change the background and color based
-//    on the given variant.
-// 🦉 remember, you don't have to make things look perfect or just like they
-// do in the final example. Just make sure you understand how to create the
-// styled component and accept a prop to change which styles apply.
+const spin = keyframes({
+  "0%": { transform: "rotate(0deg)" },
+  "100%": { transform: "rotate(360deg)" },
+});
 
-// 🐨 Feel free to create as many reusable styled components here as you'd like
-// 💰 in my finished version I have: Button, Input, CircleButton, Dialog, FormGroup
+const Spinner = styled(FaSpinner)({
+  animation: `${spin} 1s linear infinite`,
+});
+Spinner.defaultProps = {
+  "aria-label": "loading",
+};
 
-// 🎨 here are a bunch of styles you can copy/paste if you want
-// Button:
-//   padding: '10px 15px',
-//   border: '0',
-//   lineHeight: '1',
-//   borderRadius: '3px',
+const buttonVariants = {
+  primary: {
+    background: colors.indigo,
+    color: colors.base,
+  },
+  secondary: {
+    background: colors.gray,
+    color: colors.text,
+  },
+};
+const Button = styled.button(
+  {
+    padding: "10px 15px",
+    border: "0",
+    lineHeight: "1",
+    borderRadius: "3px",
+  },
+  ({ variant = "primary" }) => buttonVariants[variant]
+);
 
-// Button variant="primary" (in addition to the above styles)
-//   background: '#3f51b5',
-//   color: 'white',
+const Input = styled.input({
+  borderRadius: "3px",
+  border: `1px solid ${colors.gray10}`,
+  background: colors.gray,
+  padding: "8px 12px",
+});
 
-// Button variant="secondary" (in addition to the above styles)
-//   background: '#f1f2f7',
-//   color: '#434449',
-
-// Input
-//   borderRadius: '3px',
-//   border: '1px solid #f1f1f4',
-//   background: '#f1f2f7',
-//   padding: '8px 12px',
-
-// FormGroup
-//   display: 'flex',
-//   flexDirection: 'column',
-
-// 💰 I'm giving a few of these to you:
 const CircleButton = styled.button({
-  borderRadius: '30px',
-  padding: '0',
-  width: '40px',
-  height: '40px',
-  lineHeight: '1',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  background: 'white',
-  color: '#434449',
-  border: `1px solid #f1f1f4`,
-  cursor: 'pointer',
-})
+  borderRadius: "30px",
+  padding: "0",
+  width: "40px",
+  height: "40px",
+  lineHeight: "1",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  background: colors.base,
+  color: colors.text,
+  border: `1px solid ${colors.gray10}`,
+  cursor: "pointer",
+});
 
 const Dialog = styled(ReachDialog)({
-  maxWidth: '450px',
-  borderRadius: '3px',
-  paddingBottom: '3.5em',
-  boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
-  margin: '20vh auto',
-  '@media (max-width: 991px)': {
-    width: '100%',
-    margin: '10vh auto',
+  maxWidth: "450px",
+  borderRadius: "3px",
+  paddingBottom: "3.5em",
+  boxShadow: "0 10px 30px -5px rgba(0, 0, 0, 0.2)",
+  margin: "20vh auto",
+  [mq.small]: {
+    width: "100%",
+    margin: "10vh auto",
   },
-})
+});
 
-export {CircleButton, Dialog}
+const FormGroup = styled.div({
+  display: "flex",
+  flexDirection: "column",
+});
+
+export { Button, Input, CircleButton, Dialog, FormGroup, Spinner };
--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,114 +1,109 @@
-// 🐨 make sure to add the comment and import jsx from @emotion/core
-// up here so you can use the css prop
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 
-// 🐨 let's get a solid reset of global styles so everything looks a bit better
-// In this project we're using bootstrap-reboot which you can import from
-// bootstrap/dist/css/bootstrap-reboot.css
-// 🦉 Note: you can definitely use regular styles to style React apps
-// and using any modern toolchain will allow you to simply import the CSS file
-// but CSS-in-JS is generally easier to maintain.
-import '@reach/dialog/styles.css'
-import * as React from 'react'
-import ReactDOM from 'react-dom'
-// 🐨 you'll need to import some new components that you'll be creating
-// in this file
-// import {Button, Input, FormGroup} from './components/lib'
-import {Modal, ModalContents, ModalOpenButton} from './components/modal'
-import {Logo} from './components/logo'
+import "bootstrap/dist/css/bootstrap-reboot.css";
+import "@reach/dialog/styles.css";
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { Button, Input, FormGroup, Spinner } from "./components/lib";
+import { Modal, ModalContents, ModalOpenButton } from "./components/modal";
+import { Logo } from "./components/logo";
 
-function LoginForm({onSubmit, submitButton}) {
+function LoginForm({ onSubmit, submitButton }) {
   function handleSubmit(event) {
-    event.preventDefault()
-    const {username, password} = event.target.elements
+    event.preventDefault();
+    const { username, password } = event.target.elements;
 
     onSubmit({
       username: username.value,
       password: password.value,
-    })
+    });
   }
 
-  // 🐨 this <form> could use a css prop
-  // 🎨
-  //    display: 'flex',
-  //    flexDirection: 'column',
-  //    alignItems: 'stretch',
-  //    '> div': {
-  //      margin: '10px auto',
-  //      width: '100%',
-  //      maxWidth: '300px',
-  //    },
   return (
-    <form onSubmit={handleSubmit}>
-      {/* 🐨 these div elements could be a FormGroup you create in components/lib */}
-      {/* 🐨 and the inputs elements could be custom styled Input components too */}
-      <div>
+    <form
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "stretch",
+        "> div": {
+          margin: "10px auto",
+          width: "100%",
+          maxWidth: "300px",
+        },
+      }}
+      onSubmit={handleSubmit}
+    >
+      <FormGroup>
         <label htmlFor="username">Username</label>
-        <input id="username" />
-      </div>
-      <div>
+        <Input id="username" />
+      </FormGroup>
+      <FormGroup>
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" />
+        <Input id="password" type="password" />
+      </FormGroup>
+      <div>
+        {React.cloneElement(submitButton, { type: "submit" })}
+        <Spinner css={{ marginLeft: 5 }} />
       </div>
-      <div>{React.cloneElement(submitButton, {type: 'submit'})}</div>
     </form>
-  )
+  );
 }
 
 function App() {
   function login(formData) {
-    console.log('login', formData)
+    console.log("login", formData);
   }
 
   function register(formData) {
-    console.log('register', formData)
+    console.log("register", formData);
   }
 
-  // 🐨 this div could use a css prop to get its children rendered nicer
-  // 🎨
-  //    display: 'flex',
-  //    flexDirection: 'column',
-  //    alignItems: 'center',
-  //    justifyContent: 'center',
-  //    width: '100%',
-  //    height: '100vh',
   return (
-    <div>
+    <div
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-      {/*
-        🐨 the two buttons are too close, let's space them out
-          🎨 apply this to the div right below
-            display: 'grid',
-            gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
-            gridGap: '0.75rem',
-      */}
-      {/* 🐨 And make sure to use the new Button component for all these buttons */}
-      <div>
+      <div
+        css={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
+          gridGap: "0.75rem",
+        }}
+      >
         <Modal>
           <ModalOpenButton>
-            <button variant="primary">Login</button>
+            <Button variant="primary">Login</Button>
           </ModalOpenButton>
           <ModalContents aria-label="Login form" title="Login">
             <LoginForm
               onSubmit={login}
-              submitButton={<button variant="primary">Login</button>}
+              submitButton={<Button variant="primary">Login</Button>}
             />
           </ModalContents>
         </Modal>
         <Modal>
           <ModalOpenButton>
-            <button variant="secondary">Register</button>
+            <Button variant="secondary">Register</Button>
           </ModalOpenButton>
           <ModalContents aria-label="Registration form" title="Register">
             <LoginForm
               onSubmit={register}
-              submitButton={<button variant="secondary">Register</button>}
+              submitButton={<Button variant="secondary">Register</Button>}
             />
           </ModalContents>
         </Modal>
       </div>
     </div>
-  )
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
