16171aee57bd2ed59a2c431857711fefdda4d289
Kent C. Dodds <me+github@kentcdodds.com>
2022-06-21 11:06:35 -0600

    exercises/06-cache-management

---
+++ src/utils/list-items.extra-7.js
@@ -0,0 +1,90 @@
+import {useQuery, useMutation, queryCache} from 'react-query'
+import {setQueryDataForBook} from './books'
+import {client} from './api-client'
+
+function useListItems(user) {
+  const {data: listItems} = useQuery({
+    queryKey: 'list-items',
+    queryFn: () =>
+      client(`list-items`, {token: user.token}).then(data => data.listItems),
+    config: {
+      onSuccess(listItems) {
+        for (const listItem of listItems) {
+          setQueryDataForBook(listItem.book)
+        }
+      },
+    },
+  })
+  return listItems ?? []
+}
+
+function useListItem(user, bookId) {
+  const listItems = useListItems(user)
+  return listItems.find(li => li.bookId === bookId) ?? null
+}
+
+const defaultMutationOptions = {
+  onError: (err, variables, recover) =>
+    typeof recover === 'function' ? recover() : null,
+  onSettled: () => queryCache.invalidateQueries('list-items'),
+}
+
+function useUpdateListItem(user, ...options) {
+  return useMutation(
+    updates =>
+      client(`list-items/${updates.id}`, {
+        method: 'PUT',
+        data: updates,
+        token: user.token,
+      }),
+    {
+      onMutate(newItem) {
+        const previousItems = queryCache.getQueryData('list-items')
+
+        queryCache.setQueryData('list-items', old => {
+          return old.map(item => {
+            return item.id === newItem.id ? {...item, ...newItem} : item
+          })
+        })
+
+        return () => queryCache.setQueryData('list-items', previousItems)
+      },
+      ...defaultMutationOptions,
+      ...options,
+    },
+  )
+}
+
+function useRemoveListItem(user, options) {
+  return useMutation(
+    ({id}) => client(`list-items/${id}`, {method: 'DELETE', token: user.token}),
+    {
+      onMutate(removedItem) {
+        const previousItems = queryCache.getQueryData('list-items')
+
+        queryCache.setQueryData('list-items', old => {
+          return old.filter(item => item.id !== removedItem.id)
+        })
+
+        return () => queryCache.setQueryData('list-items', previousItems)
+      },
+      ...options,
+      ...defaultMutationOptions,
+    },
+  )
+}
+
+function useCreateListItem(user, options) {
+  return useMutation(
+    ({bookId}) => client(`list-items`, {data: {bookId}, token: user.token}),
+    {...defaultMutationOptions, ...options},
+  )
+}
+
+export {
+  useListItem,
+  useListItems,
+  useUpdateListItem,
+  useRemoveListItem,
+  useCreateListItem,
+}
