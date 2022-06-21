4a700d1d52e522c48f1ce3e2f6d240e6ad7dd2eb
Kent C. Dodds <me+github@kentcdodds.com>
2022-06-21 11:06:28 -0600

    exercises/02-styles

---
+++ src/components/lib.exercise.js
@@ -0,0 +1,71 @@
+import styled from '@emotion/styled'
+import {Dialog as ReachDialog} from '@reach/dialog'
+
+// 🐨 create a button styled component here called "Button"
+// make it look nice and allow it to support a "variant" prop which can be
+// either "primary" or "secondary".
+// 💰 don't forget to export it at the bottom!
+// 💰 In my final version, I style padding, border, lineHeight, and borderRadius
+//    the same for both types, and then change the background and color based
+//    on the given variant.
+// 🦉 remember, you don't have to make things look perfect or just like they
+// do in the final example. Just make sure you understand how to create the
+// styled component and accept a prop to change which styles apply.
+
+// 🐨 Feel free to create as many reusable styled components here as you'd like
+// 💰 in my finished version I have: Button, Input, CircleButton, Dialog, FormGroup
+
+// 🎨 here are a bunch of styles you can copy/paste if you want
+// Button:
+//   padding: '10px 15px',
+//   border: '0',
+//   lineHeight: '1',
+//   borderRadius: '3px',
+
+// Button variant="primary" (in addition to the above styles)
+//   background: '#3f51b5',
+//   color: 'white',
+
+// Button variant="secondary" (in addition to the above styles)
+//   background: '#f1f2f7',
+//   color: '#434449',
+
+// Input
+//   borderRadius: '3px',
+//   border: '1px solid #f1f1f4',
+//   background: '#f1f2f7',
+//   padding: '8px 12px',
+
+// FormGroup
+//   display: 'flex',
+//   flexDirection: 'column',
+
+// 💰 I'm giving a few of these to you:
+const CircleButton = styled.button({
+  borderRadius: '30px',
+  padding: '0',
+  width: '40px',
+  height: '40px',
+  lineHeight: '1',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  background: 'white',
+  color: '#434449',
+  border: `1px solid #f1f1f4`,
+  cursor: 'pointer',
+})
+
+const Dialog = styled(ReachDialog)({
+  maxWidth: '450px',
+  borderRadius: '3px',
+  paddingBottom: '3.5em',
+  boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
+  margin: '20vh auto',
+  '@media (max-width: 991px)': {
+    width: '100%',
+    margin: '10vh auto',
+  },
+})
+
+export {CircleButton, Dialog}
