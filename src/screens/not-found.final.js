728d2953e01abc483d340f9de91ad05f2c268285
Kent C. Dodds <me+github@kentcdodds.com>
2022-06-21 11:06:33 -0600

    exercises/05-routing

---
+++ src/screens/not-found.final.js
@@ -0,0 +1,23 @@
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+
+import {Link} from 'components/lib'
+
+function NotFoundScreen() {
+  return (
+    <div
+      css={{
+        height: '100%',
+        display: 'grid',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div>
+        Sorry... nothing here. <Link to="/discover">Go home</Link>
+      </div>
+    </div>
+  )
+}
+
+export {NotFoundScreen}
