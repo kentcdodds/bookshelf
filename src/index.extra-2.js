953f98bda5d8a5273ed50fb3b48329f30753b339
Kent C. Dodds <me+github@kentcdodds.com>
2022-06-21 11:06:35 -0600

    exercises/06-cache-management

---
+++ src/index.extra-2.js
@@ -0,0 +1,31 @@
+import {loadDevTools} from './dev-tools/load'
+import './bootstrap'
+import * as React from 'react'
+import {createRoot} from 'react-dom/client'
+import {ReactQueryConfigProvider} from 'react-query'
+import {App} from './app'
+
+const queryConfig = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry(failureCount, error) {
+      if (error.status === 404) return false
+      else if (failureCount < 2) return true
+      else return false
+    },
+  },
+}
+
+// ignore the rootRef in this file. I'm just doing it here to make
+// the tests I write to check your work easier.
+export const rootRef = {}
+loadDevTools(() => {
+  const root = createRoot(document.getElementById('root'))
+  root.render(
+    <ReactQueryConfigProvider config={queryConfig}>
+      <App />
+    </ReactQueryConfigProvider>,
+  )
+  rootRef.current = root
+})
