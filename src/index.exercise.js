ca035e67a5c5d30a4f26e10f8db67f614e6d0e98
bharathinger <bharat3987@outlook.com>
2023-01-02 18:25:30 +0530

    Initial setup - logo, heading and buttons

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,20 @@
-// 🐨 you'll need to import react and createRoot from react-dom up here
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Logo } from './components/logo'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
-
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
-
-// 🐨 use createRoot to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+const App = () => {
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => console.log('Login clicked')}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => console.log('Register clicked')}>Register</button>
+      </div>
+    </div>
+  )
+}
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
