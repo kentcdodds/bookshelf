1abbcd239a231027dd018e7464f10fcc31f1e8f0
amit-kayasth <amit@insyncit.co.uk>
2023-07-17 10:48:47 +0100

    feat: ex-01

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,4 +1,7 @@
 // 🐨 you'll need to import react and createRoot from react-dom up here
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Logo} from './components/logo'
 
 // 🐨 you'll also need to import the Logo component from './components/logo'
 
@@ -7,3 +10,19 @@
 
 // 🐨 use createRoot to render the <App /> to the root element
 // 💰 find the root element with: document.getElementById('root')
+function App() {
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => alert('Login Clicked')}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => alert('Register Clicked')}>Register</button>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
