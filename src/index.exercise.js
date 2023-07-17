b15ca809bb2968d36e4e628f70ab1a27fd212dbc
amit-kayasth <amit@insyncit.co.uk>
2023-07-17 13:40:32 +0100

    feat: ex-01-02

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,12 +1,48 @@
-// 🐨 you'll need to import react and createRoot from react-dom up here
-import React, {useState} from 'react'
-import ReactDOM from 'react-dom'
-import {Logo} from './components/logo'
 import '@reach/dialog/styles.css'
+import * as React from 'react'
+import {createRoot} from 'react-dom/client'
 import {Dialog} from '@reach/dialog'
+import {Logo} from './components/logo'
+
+function LoginForm({onSubmit, buttonText}) {
+  function handleSubmit(event) {
+    event.preventDefault()
+    const {username, password} = event.target.elements
+
+    onSubmit({
+      username: username.value,
+      password: password.value,
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input id="username" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" />
+      </div>
+      <div>
+        <button type="submit">{buttonText}</button>
+      </div>
+    </form>
+  )
+}
 
 function App() {
   const [openModal, setOpenModal] = React.useState('none')
+
+  function login(formData) {
+    console.log('login', formData)
+  }
+
+  function register(formData) {
+    console.log('register', formData)
+  }
+
   return (
     <div>
       <Logo width="80" height="80" />
@@ -22,15 +58,19 @@ function App() {
           <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
         <h3>Login</h3>
+        <LoginForm onSubmit={login} buttonText="Login" />
       </Dialog>
       <Dialog aria-label="Registration form" isOpen={openModal === 'register'}>
         <div>
           <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
         <h3>Register</h3>
+        <LoginForm onSubmit={register} buttonText="Register" />
       </Dialog>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
+export {root}
