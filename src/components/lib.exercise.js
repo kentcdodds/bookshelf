717acc5308fcf7dd60135de9d240bf7765e68532
amit-kayasth <amit@insyncit.co.uk>
2023-07-18 10:43:25 +0100

    feat: test

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -23,29 +23,32 @@ const buttonVariants = {
     background: '#3f51b5',
     color: 'white',
   },
-  secondary {
+  secondary: {
     background: '#f1f2f7',
-    color: '#434449'
-  }
+    color: '#434449',
+  },
 }
 
-const Button = styled.button({
-  padding: '10px 15px',
-  border: '0',
-  lineHeight: '1',
-  borderRadius: '3px',
-},   ({variant = 'primary'}) => buttonVariants[variant])
+const Button = styled.button(
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+  },
+  ({variant = 'primary'}) => buttonVariants[variant],
+)
 
 // Button variant="primary" (in addition to the above styles)
 
 // Button variant="secondary" (in addition to the above styles)
-  const Input = styled.input({
-    borderRadius: '3px',
-    border: '1px solid #f1f1f4',
-    background: '#f1f2f7',
-    padding: '8px 12px',
-  })
- 
+const Input = styled.input({
+  borderRadius: '3px',
+  border: '1px solid #f1f1f4',
+  background: '#f1f2f7',
+  padding: '8px 12px',
+})
+
 const FormGroup = styled.div({
   display: 'flex',
   flexDirection: 'column',
@@ -67,17 +70,16 @@ const CircleButton = styled.button({
   cursor: 'pointer',
 })
 
-const Dialog = styled(ReachDialog)(
-  {
-    maxWidth: '450px',
-    borderRadius: '3px',
-    paddingBottom: '3.5em',
-    boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
-    margin: '20vh auto',
-    '@media (max-width: 991px)': {
-      width: '100%',
-      margin: '10vh auto',
-    },
-  })
+const Dialog = styled(ReachDialog)({
+  maxWidth: '450px',
+  borderRadius: '3px',
+  paddingBottom: '3.5em',
+  boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
+  margin: '20vh auto',
+  '@media (max-width: 991px)': {
+    width: '100%',
+    margin: '10vh auto',
+  },
+})
 
 export {Button, Input, CircleButton, Dialog, FormGroup}
