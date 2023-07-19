49f7d02ecf8116961f3edc1c6ec3a79826bf159c
amit-kayasth <amit@insyncit.co.uk>
2023-07-19 12:34:12 +0100

    feat: ex-01 to ex-04

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,18 +1,28 @@
+import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {data, token, headers: customHeaders, ...customConfig} = {},
 ) {
   const config = {
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': data ? 'application/json' : undefined,
       ...customHeaders,
     },
     ...customConfig,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      await auth.logout()
+      // refresh the page for them
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please re-authenticate.'})
+    }
     const data = await response.json()
     if (response.ok) {
       return data
