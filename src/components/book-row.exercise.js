75012cc56141d39defc5a6e81e9c9fa281761e39
amit-kayasth <amit@insyncit.co.uk>
2023-07-21 14:36:31 +0100

    feat: react routing complete section

--- src/components/book-row.exercise.js
+++ src/components/book-row.exercise.js
@@ -1,8 +1,7 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
 
-// We're going to turn the entire book row into a link to the book page
-// 🐨 import the Link component from react-router-dom
+import {Link} from 'react-router-dom'
 import * as mq from 'styles/media-queries'
 import * as colors from 'styles/colors'
 
@@ -20,12 +19,9 @@ function BookRow({book}) {
         position: 'relative',
       }}
     >
-      {/*
-          🐨 Turn this div into a Link
-          and add a to prop to make it direct to `/book/${book.id}`
-      */}
-      <div
+      <Link
         aria-labelledby={id}
+        to={`/book/${book.id}`}
         css={{
           minHeight: 270,
           flexGrow: 2,
@@ -88,7 +84,7 @@ function BookRow({book}) {
             {book.synopsis.substring(0, 500)}...
           </small>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
--- src/screens/book.exercise.js
+++ src/screens/book.exercise.js
@@ -2,8 +2,7 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// we need to get the "bookId" param from the router
-// 🐨 import the useParams hook from 'react-router-dom'
+import {useParams} from 'react-router-dom'
 import {client} from 'utils/api-client'
 import * as mq from 'styles/media-queries'
 import {useAsync} from 'utils/hooks'
@@ -19,12 +18,7 @@ const loadingBook = {
 }
 
 function BookScreen({user}) {
-  // 🐨 use the useParams hook. This'll give you back an object with all the
-  // params you've specified in the route definition. You should be able to get
-  // the bookId from that.
-
-  // 💣 remove this, we're getting the bookId from useParams instead.
-  const bookId = '??'
+  const {bookId} = useParams()
   const {data, run} = useAsync()
 
   React.useEffect(() => {
