174d786f5f9def070737a58ecf68c0caf1c5b93d
amit-kayasth <amit@insyncit.co.uk>
2023-08-02 19:25:35 +0100

    feat: ex-07-06

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,19 +1,21 @@
-// 🐨 you don't need to do anything for the exercise, but there's an extra credit!
 import {loadDevTools} from './dev-tools/load'
 import './bootstrap'
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
 import {ReactQueryConfigProvider} from 'react-query'
+import {AuthProvider} from './context/auth-context'
 import {App} from './app'
 
 const queryConfig = {
-  retry(failureCount, error) {
-    if (error.status === 404) return false
-    else if (failureCount < 2) return true
-    else return false
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry(failureCount, error) {
+      if (error.status === 404) return false
+      else if (failureCount < 2) return true
+      else return false
+    },
   },
-  useErrorBoundary: true,
-  refetchAllOnWindowFocus: false,
 }
 
 // ignore the rootRef in this file. I'm just doing it here to make
@@ -23,7 +25,9 @@ loadDevTools(() => {
   const root = createRoot(document.getElementById('root'))
   root.render(
     <ReactQueryConfigProvider config={queryConfig}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ReactQueryConfigProvider>,
   )
   rootRef.current = root
