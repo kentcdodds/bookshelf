908efe5b2242ae0ca517e5d5c235cde98c29e6f3
amit-kayasth <amit@insyncit.co.uk>
2023-08-02 19:39:49 +0100

    feat: ex-07-03

--- src/context/index.exercise.js
+++ src/context/index.exercise.js
@@ -1,2 +1,27 @@
-// this module doesn't do anything for the exercise. But you'll use this for
-// the extra credit!
+import * as React from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {ReactQueryConfigProvider} from 'react-query'
+import {AuthProvider} from './auth-context'
+
+const queryConfig = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry(failureCount, error) {
+      if (error.status === 404) return false
+      else if (failureCount < 2) return true
+      else return false
+    },
+  },
+}
+function AppProviders({children}) {
+  return (
+    <ReactQueryConfigProvider config={queryConfig}>
+      <Router>
+        <AuthProvider>{children}</AuthProvider>
+      </Router>
+    </ReactQueryConfigProvider>
+  )
+}
+
+export {AppProviders}
