cc468ff306435e797c35cd84dcc3fc58f9fbfc6d
amit-kayasth <amit@insyncit.co.uk>
2023-08-07 14:06:42 +0100

    -09-03-01

--- src/components/profiler.exercise.js
+++ src/components/profiler.exercise.js
@@ -1,1 +1,43 @@
-// this is for extra credit
+import * as React from 'react'
+import {client} from 'utils/api-client'
+
+let queue = []
+
+setInterval(sendProfileQueue, 5000)
+
+function sendProfileQueue() {
+  if (!queue.length) {
+    return Promise.resolve({success: true})
+  }
+  const queueToSend = [...queue]
+  queue = []
+  return client('profile', {data: queueToSend})
+}
+
+function Profiler({metadata, phases, ...props}) {
+  function reportProfile(
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions,
+  ) {
+    if (!phases || phases.includes(phase)) {
+      queue.push({
+        metadata,
+        id,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+        interactions,
+      })
+    }
+  }
+  return <React.Profiler onRender={reportProfile} {...props} />
+}
+
+export {Profiler}
