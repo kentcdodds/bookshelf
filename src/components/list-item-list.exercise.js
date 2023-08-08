f06278c299c1aec8980dee92983a83b7f1b70985
amit-kayasth <amit@insyncit.co.uk>
2023-08-08 18:20:27 +0100

    feat: ex-09-complete

--- src/components/list-item-list.exercise.js
+++ src/components/list-item-list.exercise.js
@@ -4,6 +4,7 @@ import {jsx} from '@emotion/core'
 import {useListItems} from 'utils/list-items'
 import {BookListUL} from './lib'
 import {BookRow} from './book-row'
+import {Profiler} from './profiler'
 
 function ListItemList({filterListItems, noListItems, noFilteredListItems}) {
   const listItems = useListItems()
@@ -22,13 +23,18 @@ function ListItemList({filterListItems, noListItems, noFilteredListItems}) {
   }
 
   return (
-    <BookListUL>
-      {filteredListItems.map(listItem => (
-        <li key={listItem.id} aria-label={listItem.book.title}>
-          <BookRow book={listItem.book} />
-        </li>
-      ))}
-    </BookListUL>
+    <Profiler
+      id="List Item List"
+      metadata={{listItemCount: filteredListItems.length}}
+    >
+      <BookListUL>
+        {filteredListItems.map(listItem => (
+          <li key={listItem.id} aria-label={listItem.book.title}>
+            <BookRow book={listItem.book} />
+          </li>
+        ))}
+      </BookListUL>
+    </Profiler>
   )
 }
 
--- src/screens/discover.exercise.js
+++ src/screens/discover.exercise.js
@@ -8,6 +8,7 @@ import * as colors from 'styles/colors'
 import {useBookSearch, useRefetchBookSearchQuery} from 'utils/books'
 import {BookRow} from 'components/book-row'
 import {BookListUL, Spinner, Input} from 'components/lib'
+import {Profiler} from 'components/profiler'
 
 function DiscoverBooksScreen() {
   const [query, setQuery] = React.useState('')
@@ -84,13 +85,18 @@ function DiscoverBooksScreen() {
           </div>
         )}
         {books.length ? (
-          <BookListUL css={{marginTop: 20}}>
-            {books.map(book => (
-              <li key={book.id} aria-label={book.title}>
-                <BookRow key={book.id} book={book} />
-              </li>
-            ))}
-          </BookListUL>
+          <Profiler
+            id="Discover Books Screen Book List"
+            metadata={{query, bookCount: books.length}}
+          >
+            <BookListUL css={{marginTop: 20}}>
+              {books.map(book => (
+                <li key={book.id} aria-label={book.title}>
+                  <BookRow key={book.id} book={book} />
+                </li>
+              ))}
+            </BookListUL>
+          </Profiler>
         ) : queried ? (
           <div css={{marginTop: 20, fontSize: '1.2em', textAlign: 'center'}}>
             {isLoading ? (
